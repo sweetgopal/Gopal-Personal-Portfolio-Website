1e9473bf698532fed79c40b4c4fd41a289889537
sweetgopal <sweetgopal@gmail.com>
2021-07-12 20:59:29 -0400

    Wording Changes in Portfolio website
    Wording Changes in Portfolio website

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script on top bar of the Web-site
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script/[X] button on the menu bar
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["QE&A Expert", "Automation Expert", "Blogger", "Transformation Specialist", "Quality Process Expert"],
+        strings: ["QE&A Expert", "Automation Expert", "Delivery Specialist", "Digital Transformation Specialist", "Quality Process Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["QE&A Expert", "Automation Expert", "Blogger", "Transformation Specialist", "Quality Process Expert"],
+        strings: ["QE&A Expert", "Automation Expert", "Delivery Specialist", "Digital Transformation Specialist", "Quality Process Expert"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -56,15 +56,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
